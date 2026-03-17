24b564243b66492d0044b2cc181ae4638e633277
Andrew Wang <andrewwangpa@gmail.com>
2026-03-17 16:33:25 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -169,7 +169,7 @@
 			//message = `X: ${x}, Y: ${y}, Z: ${z}`;
 			if (y > 0) {
 				message = `X: ${x}, Y: ${y}, Z: ${z}`;
-				gravity.y = MAX_GRAVITY * deltaY * 1000000;
+				gravity.y = MAX_GRAVITY * deltaY * 10000;
 			}
 			if (y < 0) {
 				gravity.y = -MAX_GRAVITY * deltaY * 1000;
