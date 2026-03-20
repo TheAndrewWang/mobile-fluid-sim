940658120ae19205db43b564af78f03d4a49ef30
Vincent Huang <vincenth0404@gmail.com>
2026-03-20 16:52:39 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -92,7 +92,7 @@
 	let lastShakeTime = 0;
 	let lastAcceleration = { x: 0, y: 0, z: 0 };
 	let shakeThreshold = 10;
-	let shakeTimeThreshold = 500;
+	let shakeTimeThreshold = 1000;
 
 	const requestPermission = async () => {
 		if (!browser) return;
@@ -174,6 +174,7 @@
 			if (y < 0) {
 				message = `X: ${x}, Y: ${y}, Z: ${z}`;
 				gravity.y = MAX_GRAVITY * (-y);
+				gravity.x = 0;
 				message4 = `XG: ${gravity.x}, YG: ${gravity.y}`;
 			}
 			/*
@@ -183,7 +184,7 @@
 
 		}
 
-		if (totalDelta < shakeThreshold) {
+		if (totalDelta < shakeThreshold && currentTime - lastShakeTime > shakeTimeThreshold) {
 			MAX_GRAVITY = 50.81;
 			message3 = `G: ${MAX_GRAVITY}`;
 		}
